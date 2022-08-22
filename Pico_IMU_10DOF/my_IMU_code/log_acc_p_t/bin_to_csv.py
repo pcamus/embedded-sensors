ec7b73851f7e444885d8fd1c4c17e15b0b1049bd
Philippe CAMUS <55027870+pcamus@users.noreply.github.com>
2022-08-22 13:08:01 +0200

    Processes the whole file now.

--- Pico_IMU_10DOF/my_IMU_code/log_acc_p_t/bin_to_csv.py
+++ Pico_IMU_10DOF/my_IMU_code/log_acc_p_t/bin_to_csv.py
@@ -5,8 +5,6 @@
 # Converts acceleration and pressure+temperature binary files
 # into csv files
 #
-# Only for one 10 s sample. To be extended to full file
-#
 # info@pcamus.be
 # 22/8/2022
 
@@ -17,40 +15,48 @@ filn_acc="acc.bin"
 filn_PT="Press_Temp.bin"
 
 # Reads the first 10 sec acceleration log
-f = open(filn_acc, "rb")
-file_buf_acc=f.read(600)
-f.close()
+f_acc_b = open(filn_acc, "rb")
+file_buf_acc=f_acc_b.read(600)
+
+f_acc_csv = open(filn_acc_csv, "w")
+
+# While there is something to read
+while len(file_buf_acc)==600:
+    # Process acceleration samples
+    for i in range(100):
+        buf_index=i*6
+        acc_x=file_buf_acc[buf_index]*256+file_buf_acc[buf_index+1]
+        if acc_x>=32767:             
+            acc_x=acc_x-65535
+
+        acc_y=file_buf_acc[buf_index+2]*256+file_buf_acc[buf_index+3]
+        if acc_y>=32767:             
+            acc_y=acc_y-65535
+            
+        acc_z=file_buf_acc[buf_index+4]*256+file_buf_acc[buf_index+5]
+        if acc_z>=32767:             
+            acc_z=acc_z-65535
+        
+        f_acc_csv.write("%d;%d;%d\n"%(acc_x,acc_y,acc_z))    
+
+    file_buf_acc=f_acc_b.read(600)    
+
+f_acc_b.close()
+f_acc_csv.close()
 
 # Reads the first pressure and temperature log
-f = open(filn_PT, "rb")
-file_buf_PT=f.read(4)
-f.close()
+f_PT_b = open(filn_PT, "rb")
+file_buf_PT=f_PT_b.read(4)
 
-f = open(filn_acc_csv, "w")
+f_PT_csv = open(filn_PT_csv, "w")
 
-# Process acceleration samples
-for i in range(100):
-    buf_index=i*6
-    acc_x=file_buf_acc[buf_index]*256+file_buf_acc[buf_index+1]
-    if acc_x>=32767:             
-        acc_x=acc_x-65535
+# While there is something to read
+while len(file_buf_PT)==4:
+    # Process pressure and temperature samples
+    pressure = file_buf_PT[0]*256+file_buf_PT[1]
+    temperature = (file_buf_PT[2]*256+file_buf_PT[3])/10
+    f_PT_csv.write("%d;%4.1f\n"%(pressure, temperature))
+    file_buf_PT=f_PT_b.read(4)
 
-    acc_y=file_buf_acc[buf_index+2]*256+file_buf_acc[buf_index+3]
-    if acc_y>=32767:             
-        acc_y=acc_y-65535
-        
-    acc_z=file_buf_acc[buf_index+4]*256+file_buf_acc[buf_index+5]
-    if acc_z>=32767:             
-        acc_z=acc_z-65535
-    
-    f.write("%d;%d;%d\n"%(acc_x,acc_y,acc_z))
-    
-f.close();
-
-# Process pressure and temperature samples
-pressure = file_buf_PT[0]*256+file_buf_PT[1]
-temperature = (file_buf_PT[2]*256+file_buf_PT[3])/10
-
-f = open(filn_PT_csv, "w")
-f.write("%d;%4.1f\n"%(pressure, temperature))
-f.close()
+f_PT_b.close()
+f_PT_csv.close()
