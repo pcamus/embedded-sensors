3aca4e47106840dbc699caca14b18f6b303aa913
Philippe CAMUS <55027870+pcamus@users.noreply.github.com>
2022-08-25 10:37:09 +0200

    Add files via upload

--- Pico_IMU_10DOF/my_IMU_code/log_acc_p_t/bin_to_csv.py
+++ Pico_IMU_10DOF/my_IMU_code/log_acc_p_t/bin_to_csv.py
@@ -1,5 +1,5 @@
 # File : bin_to_csv.py
-# to be used with icm20948_lps22hb_speed_test.py program
+# to be used with acc_pt_log.py program
 # and other similar programs.
 #
 # Converts acceleration and pressure+temperature binary files
@@ -14,32 +14,30 @@ filn_PT_csv="Press_Temp.csv"
 filn_acc="acc.bin"
 filn_PT="Press_Temp.bin"
 
-# Reads the first 10 sec acceleration log
+# Reads the first acceleration sample
 f_acc_b = open(filn_acc, "rb")
-file_buf_acc=f_acc_b.read(600)
+file_buf_acc=f_acc_b.read(6)
 
 f_acc_csv = open(filn_acc_csv, "w")
 
 # While there is something to read
-while len(file_buf_acc)==600:
+while len(file_buf_acc)==6:
     # Process acceleration samples
-    for i in range(100):
-        buf_index=i*6
-        acc_x=file_buf_acc[buf_index]*256+file_buf_acc[buf_index+1]
-        if acc_x>=32767:             
-            acc_x=acc_x-65535
+    acc_x=file_buf_acc[0]*256+file_buf_acc[1]
+    if acc_x>=32767:             
+        acc_x=acc_x-65535
 
-        acc_y=file_buf_acc[buf_index+2]*256+file_buf_acc[buf_index+3]
-        if acc_y>=32767:             
-            acc_y=acc_y-65535
-            
-        acc_z=file_buf_acc[buf_index+4]*256+file_buf_acc[buf_index+5]
-        if acc_z>=32767:             
-            acc_z=acc_z-65535
+    acc_y=file_buf_acc[2]*256+file_buf_acc[3]
+    if acc_y>=32767:             
+        acc_y=acc_y-65535
         
-        f_acc_csv.write("%d;%d;%d\n"%(acc_x,acc_y,acc_z))    
+    acc_z=file_buf_acc[4]*256+file_buf_acc[5]
+    if acc_z>=32767:             
+        acc_z=acc_z-65535
+    
+    f_acc_csv.write("%d;%d;%d\n"%(acc_x,acc_y,acc_z))    
 
-    file_buf_acc=f_acc_b.read(600)    
+    file_buf_acc=f_acc_b.read(6)    
 
 f_acc_b.close()
 f_acc_csv.close()
